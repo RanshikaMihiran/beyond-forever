a50e7f0255c5abef623a2fb3c883146d6a3d8935
Ranshika  Mihiran <ranshikam.consumer@HEMAS.LK>
2026-03-10 13:45:30 +0530

    Update FallingMemories slider for mobile and desktop

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,10 +4,17 @@ import { Camera, Users, MapPin, Coffee } from 'lucide-react';
 // src/data/constants.js
 
 export const HOME_HERO_IMAGES = [
-  "/images/hero/Image 1.jpg",
-  "/images/hero/Image 2.jpg",
-  "/images/hero/Image 3.jpg",
-  "/images/hero/Image 4.jpg",
+  // "/images/hero/Image 1.jpg",
+  // "/images/hero/Image 2.jpg",
+  // "/images/hero/Image 3.jpg",
+  // "/images/hero/Image 4.jpg",
+  "https://ik.imagekit.io/vaibbbrnqt/beyond-forever/Pre-2/SHA09127.webp?updatedAt=1772638960495",
+  "https://ik.imagekit.io/vaibbbrnqt/beyond-forever/Pre-2/SHA08790.webp?updatedAt=1772638977261",
+  "https://ik.imagekit.io/vaibbbrnqt/beyond-forever/Night-Wedding%20/SHA02855.webp?updatedAt=1772638178985",
+  "https://ik.imagekit.io/vaibbbrnqt/beyond-forever/Cover-Images/DSC00818.webp?updatedAt=1772205156455",
+  "https://ik.imagekit.io/vaibbbrnqt/beyond-forever/Night-Wedding%20/5.webp?updatedAt=1772638161356",
+  "https://ik.imagekit.io/vaibbbrnqt/beyond-forever/Preshoot/2.webp?updatedAt=1772645003741",
+  "https://ik.imagekit.io/vaibbbrnqt/beyond-forever/Pre-2/SHA09127.webp?updatedAt=1772638960495",
   "/images/hero/Image 5.jpg"
 ];
 
@@ -38,8 +45,12 @@ export const PORTFOLIO_PROJECTS = [
   { id: 6, title: "Traditional", category: "Event",    src: "/images/portfolio/Image 6.jpg" },
 ];
 
+
+
+/*Esthetic Section Images */
+
 export const FALLING_GALLERY = [
-  { src: "/images/portfolio/Image 1.jpg", label: "The Beginning" },
+  { src: "https://ik.imagekit.io/vaibbbrnqt/beyond-forever/Preshoot/SHA05957.webp?updatedAt=1772645055696", label: "Isuru & Naduni" },
   { src: "/images/portfolio/Image 2.jpg", label: "Pure Joy" },
   { src: "/images/portfolio/Image 3.jpg", label: "Details" },
   { src: "/images/portfolio/Image 4.jpg", label: "Forever" },
@@ -56,6 +67,8 @@ export const DESTINATIONS = [
   { id: 2, place: "Hill Country", desc: "Misty tea gardens and waterfalls.", img: "/images/destinations/Image 2.jpg" },
   { id: 3, place: "South Coast", desc: "Golden beaches and coconut groves.", img: "/images/destinations/Image 3.jpg" }
 ];
+
+
 
 
 
